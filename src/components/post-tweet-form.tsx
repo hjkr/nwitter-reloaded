f98b7d59abd144d09f2f462cad35a7314da30aaa
Sue Lee <sue@Sueui-MacBookAir.local>
2024-11-02 12:02:19 +1300

    complete UI

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,7 +1,8 @@
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, updateDoc } from "firebase/firestore";
 import { useState } from "react";
 import styled from "styled-components"
-import { auth, db } from "../firebase";
+import { auth, db, storage } from "../firebase";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 const Form = styled.form`
   display: flex;
@@ -63,8 +64,13 @@ export default function PostTweetForm(){
   const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { files } = e.target;
 
-    if (files && files.length == 1) {
-      setFile(files[0])
+    if (files && files.length == 1)  {
+      if(files[0].size <= 1*1024*1024)
+        setFile(files[0])
+      else {
+        alert("file size should be less then 1mb!!")
+        return;
+      }
     }
   }
 
@@ -76,12 +82,24 @@ export default function PostTweetForm(){
 
     try {
       setloading(true);
-      await addDoc(collection(db, "tweets"),{
+      const doc = await addDoc(collection(db, "tweets"),{
         tweet,
         createdAt: Date.now(),
         username: user.displayName || "Anonymous",
         userId: user.uid,
       });
+
+      if(file){
+        const locationRef = ref(storage,`tweets/${user.uid}/${doc.id}`);
+        const result = await uploadBytes(locationRef, file);
+        const url = await getDownloadURL(result.ref);
+        await updateDoc(doc,{
+          photo:url
+        } )
+      }
+      setWteet("");
+      setFile(null);
+
     } catch(e) {
       console.log(e);
     } finally {
@@ -91,6 +109,7 @@ export default function PostTweetForm(){
 
   return <Form onSubmit={onSubmit}>
     <TextArea 
+      required
       rows={5}
       maxLength={180}
       onChange={onChange} 
